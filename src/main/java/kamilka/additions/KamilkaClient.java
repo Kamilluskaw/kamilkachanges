f76469a2561306e4a231509224bbc0b14568af09
Kamilluskaw <frelluska@gmail.com>
2025-05-17 17:49:14 +0200

    basefiles

---
+++ src/main/java/kamilka/additions/KamilkaClient.java
@@ -0,0 +1,10 @@
+package kamilka.additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KamilkaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
